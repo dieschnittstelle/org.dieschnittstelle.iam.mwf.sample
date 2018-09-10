905f5246380acb14e1548dea8575e77b02747fec
jk <joern.kreutel@beuth-hochschule.de>
2018-09-10 12:22:21 +0200

    added experimental support for floating action buttons

--- webserver.js
+++ webserver.js
@@ -19,7 +19,7 @@ var server;
 // the HTTPS server
 var httpsServer;
 // the port on which the server will be started
-var port = 8384;
+var port = 6363;
 // the ip address
 var ip = utils.getIPAddress();
 // the segment for identifying the rest api
